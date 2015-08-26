7b655e5e315d7890c14a434420c4521fa431d798
islaytitans <jonathan.m.robbins@gmail.com>
2015-08-26 11:08:05 +0100

    Dev / Missing components

---
+++ JonathanRobbins.MicrositeKit.WebApp/Layouts/Sublayouts/Components/BlogDetails.ascx
@@ -0,0 +1,13 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="BlogDetails.ascx.cs" Inherits="JonathanRobbins.MicrositeKit.WebApp.Layouts.Sublayouts.Components.BlogDetails" %>
+
+<sc:Date runat="server" ID="sctDate" Field="Date" Format="ddd d MMMM" />
+<sc:Text runat="server" ID="sctTitle" Field="Title" />
+<sc:Text runat="server" ID="sctStrapline" Field="Strapline" />
+<sc:Text runat="server" ID="sctShortText" Field="Short text" />
+<sc:Text runat="server" ID="sctMainText" Field="Main text" />
+<sc:Text runat="server" ID="sctImage" Field="Image" />
+
+
+
+
+
---
+++ JonathanRobbins.MicrositeKit.WebApp/Layouts/Sublayouts/Components/BlogDetails.ascx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.UI;
+using JonathanRobbins.MicrositeKit.WebApp.Layouts.Sublayouts.ControlBases;
+using Sitecore.Data.Items;
+
+namespace JonathanRobbins.MicrositeKit.WebApp.Layouts.Sublayouts.Components
+{
+    public partial class BlogDetails : MicrositeSublayoutBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetUpLabels();
+            if (!Page.IsPostBack)
+            {
+                
+            }
+        }
+
+        private void SetUpLabels()
+        {
+            string blogItemId = ApplyParameterIfPresent(QueryStringNames.Guid);
+
+            if (!string.IsNullOrEmpty(blogItemId))
+            {
+                Item blogItem = Sitecore.Context.Database.GetItem(blogItemId);
+                if (blogItem == null) return;
+
+                sctDate.Item = blogItem;
+                sctTitle.Item = blogItem;
+                sctStrapline.Item = blogItem;
+                sctShortText.Item = blogItem;
+                sctMainText.Item = blogItem;
+                sctImage.Item = blogItem;
+            }
+        }
+    }
+}
