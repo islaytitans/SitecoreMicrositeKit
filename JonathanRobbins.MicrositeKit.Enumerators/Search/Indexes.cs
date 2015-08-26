29f6440f3bcdd739046277a9f39655f01733dc41
islaytitans <jonathan.m.robbins@gmail.com>
2015-08-26 11:46:16 +0100

    DEv / Search indexes enums

---
+++ JonathanRobbins.MicrositeKit.Enumerators/Search/Indexes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JonathanRobbins.MicrositeKit.Enumerators.Search
+{
+    public class Indexes
+    {
+        public static string Advanced = "advanced";
+        public static string AdvancedMedia = "advancedMedia";
+        public static string AdvancedEvents = "advancedEvents";
+        public static string AdvancedNews = "advancedNews";
+        public static string AdvancedBlogs = "advancedBlogs";
+        public static string AdvancedComments = "advancedComments";
+        public static string AdvancedEventsBlogsNews = "advancedEventsBlogsNews";
+        public static string Web = "sitecore_web_index";
+        public static string Site = "site";
+    }
+}
