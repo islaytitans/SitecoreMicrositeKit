51ac6dccd98ac12e23da1050f1414351f66a86b5
islaytitans <jonathan.m.robbins@gmail.com>
2015-08-27 11:19:28 +0100

    Dev / Addition of a GetDatasourceLocations pipeline to handle mulitple datasource locations for a sublayout as well as queries for that sublayout

---
+++ JonathanRobbins.MicrositeKey.CMS/Pipelines/GetRenderingDatasource/GetDatasourceLocations.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.GetRenderingDatasource;
+using Sitecore.Text;
+
+namespace JonathanRobbins.MicrositeKit.CMS.Pipelines.GetRenderingDatasource
+{
+    public class GetDatasourceLocations
+    {
+        private string DatasourceLocation { get; set; }
+        private string ContextItemPath { get; set; }
+        private Database ContextDatabase { get; set; }
+        private List<Item> DatasourceLocations { get; set; }
+        private const string QueryPrefix = "query:.";
+
+        private bool DataSourceLocationContainsQuery
+        {
+            get
+            {
+                return DatasourceLocation.Contains(QueryPrefix);
+            }
+        }
+
+        public void Process(GetRenderingDatasourceArgs args)
+        {
+            Assert.IsNotNull(args, "args");
+
+            DatasourceLocation = args.RenderingItem["Datasource Location"];
+            ContextItemPath = args.ContextItemPath;
+            ContextDatabase = args.ContentDatabase;
+            DatasourceLocations = args.DatasourceRoots;
+
+            if (DataSourceLocationContainsQuery)
+            {
+                ProcessQuerys();
+            }
+        }
+
+        private void ProcessQuerys()
+        {
+            var queries = new ListString(DatasourceLocation);
+            foreach (string query in queries)
+            {
+                if (query.StartsWith(QueryPrefix))
+                {
+                    ProcessQuery(query);
+                }
+            }
+        }
+
+        private void ProcessQuery(string query)
+        {
+            var datasourceLocations = GetDatasourceLocationsFromQuery(query).ToList();
+            if (datasourceLocations.Any())
+            {
+                foreach (Item datasourceLocation in datasourceLocations)
+                {
+                    if (!DatasourceLocations.Exists(x => x.ID.Equals(datasourceLocation.ID)))
+                    {
+                        DatasourceLocations.Add(datasourceLocation);
+                    }
+                }
+            }
+        }
+
+        private IEnumerable<Item> GetDatasourceLocationsFromQuery(string query)
+        {
+            string queryPath = query.Replace(QueryPrefix, ContextItemPath);
+            return ContextDatabase.SelectItems(queryPath);
+        }
+    }
+}
